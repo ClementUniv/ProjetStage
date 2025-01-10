eee81e334bc8b222c82c4542c342b8226842dbcc
ClemDW <clementdewasch54@gmail.com>
2025-01-10 19:19:44 +0100

    Design de la page de credits

--- ProjetStage/src/pages/Credits.jsx
+++ ProjetStage/src/pages/Credits.jsx
@@ -3,12 +3,21 @@ import "../styles/Credits.css";
 function Credits() {
     return (
         <div className="credits-page">
-            <h1>Crédits</h1>
-            <ul>
-                <li>Développeur : Clément DE WASCH</li>
-                <li>Bibliothèques utilisées : React, Vite</li>
-                <li>Site réalisé avec l'aide de la doc officielle de ReactJS</li>
-            </ul>
+            <img src="/react.svg" alt="Logo React" className="rotating-icon"/>
+            <div className="text-overlay">
+                <h1>Crédits</h1>
+                <p>
+                    Développeur : Clément DE WASCH
+                </p>
+                <p>
+                    Bibliothèques utilisées : React, Vite
+                </p>
+            </div>
+            <div className="text-overlay">
+                <p>Site réalisé en tant qu'exercice de développement en ReactJS / HTML / CSS, <br></br>
+                    avec l'aide de la doc officielle de ReactJS ainsi que UIVerse.io pour le switch</p>
+            </div>
+            <img src="/vite.svg" className="vite-icon"></img>
         </div>
     );
 }
